e3d552bd2936afcfbfe6f2f7e36f49961586309d
enzoaimebtssio <enzo.aimebtssio1@gmail.com>
2021-11-02 03:17:52 +0100

    CV en fonction du mode

--- assets/js/themes.js
+++ assets/js/themes.js
@@ -1,4 +1,10 @@
 const checkbox = document.getElementById("checkbox")
 checkbox.addEventListener("change", () => {
-  document.body.classList.toggle("dark")
+  if(document.body.classList.toggle("dark") != null){
+    document.getElementById("cv").href="docs/CVEnzodark.pdf";
+
+  }
+  else if(document.body.classList.toggle("dark")==false){
+    document.getElementById("cv").href="docs/CVEnzo.pdf";
+  }
 })
