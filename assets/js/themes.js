1b818649a3c2bd133fca84cf9e9a56de5cfbbf45
EnzoSio <76437510+EnzoSio@users.noreply.github.com>
2021-11-08 18:32:28 +0100

    modif pdf version couleur

--- assets/js/themes.js
+++ assets/js/themes.js
@@ -1,10 +1,10 @@
 const checkbox = document.getElementById("checkbox")
 checkbox.addEventListener("change", () => {
   if(document.body.classList.toggle("dark")){
-    document.getElementById("cv").href="docs/CVEnzodark.pdf";
+    document.getElementById("cv").href="./docs/CVEnzodark.pdf";
 
   }
   else if(document.body.classList.toggle!=("dark")){
-    document.getElementById("cv").href="docs/CVEnzo.pdf";
+    document.getElementById("cv").href="./docs/CVEnzo.pdf";
   }
 })
