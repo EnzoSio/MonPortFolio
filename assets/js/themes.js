68e80a99def590de7c633f30256dbe994ac70063
EnzoSio <76437510+EnzoSio@users.noreply.github.com>
2021-11-26 00:19:53 +0100

    Correctif CV

--- assets/js/themes.js
+++ assets/js/themes.js
@@ -2,9 +2,12 @@ const checkbox = document.getElementById("checkbox")
 checkbox.addEventListener("change", () => {
   if(document.body.classList.toggle("dark")){
     document.getElementById("cv").href="./docs/CVEnzodark.pdf";
+    document.getElementById("cv_qrcode").src="./docs/cv_qrcode_sombre.png";
+
 
   }
   else if(document.body.classList.toggle!=("dark")){
     document.getElementById("cv").href="./docs/CVEnzo.pdf";
+    document.getElementById("cv_qrcode").src="./docs/cv_qrcode.png";
   }
 })
