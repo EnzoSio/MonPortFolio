a86726b0737f263ec73273b064a232c40c7772df
EnzoSio <76437510+EnzoSio@users.noreply.github.com>
2021-11-29 23:15:12 +0100

    dernière modification

--- assets/js/themes.js
+++ assets/js/themes.js
@@ -21,5 +21,10 @@ if (hours > 18 || hours < 8 ) // entre 8h et 18 heures
   document.body.classList.toggle("dark")
   document.getElementById("cv").href="./docs/CVEnzodark.pdf";
   document.getElementById("cv_qrcode").src="./docs/cv_qrcode_sombre.png";
-
+}
+else {
+  document.getElementById("checkbox")
+  document.body.classList.toggle!=("dark")
+  document.getElementById("cv").href="./docs/CVEnzo.pdf";
+  document.getElementById("cv_qrcode").src="./docs/cv_qrcode.png";
 }
