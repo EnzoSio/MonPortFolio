10d66058ae924cb8346aa25a97795ee6ee831f71
EnzoSio <76437510+EnzoSio@users.noreply.github.com>
2022-03-18 12:43:50 +0100

    Modifications Cartes Veilles

--- assets/js/cartes.js
+++ assets/js/cartes.js
@@ -1,32 +1,27 @@
-$.fn.commentcartes = function(){
+// tabs
 
-    return this.each(function(){
-  
-      var $this = $(this),
-          $cartes = $this.find('.option'),
-          $current = $cartes.filter('.carte--current'),
-          $next;
-  
-      $cartes.on('click',function(){
-        if ( !$current.is(this) ) {
-          $cartes.removeClass('option--current option--out option--next');
-          $current.addClass('option--out');
-          $current = $(this).addClass('option--current');
-          $next = $current.next();
-          $next = $next.length ? $next : $cartes.first();
-          $next.addClass('option--next');
-        }
-      });
-  
-      if ( !$current.length ) {
-        $current = $cartes.last();
-        $cartes.first().trigger('click');
-      }
-  
-      $this.addClass('cartes--active');
-  
-    })
-  
-  };
-  
-  $('.cartes-options').commentcartes();
+var tabLinks = document.querySelectorAll(".tablinks");
+var tabContent = document.querySelectorAll(".tabcontent");
+
+
+tabLinks.forEach(function(el) {
+   el.addEventListener("click", openTabs);
+});
+
+
+function openTabs(el) {
+   var btnTarget = el.currentTarget;
+   var veille = btnTarget.dataset.veille;
+
+   tabContent.forEach(function(el) {
+      el.classList.remove("active");
+   });
+
+   tabLinks.forEach(function(el) {
+      el.classList.remove("active");
+   });
+
+   document.querySelector("#" + veille).classList.add("active");
+   
+   btnTarget.classList.add("active");
+}
