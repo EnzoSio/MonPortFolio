c4b146ec041ef9e3cf8cf080716b637916d03366
EnzoSio <76437510+EnzoSio@users.noreply.github.com>
2022-05-19 12:04:15 +0200

    Ajout des messages d'envoi formulaire

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -4,17 +4,16 @@ document.getElementById('form')
  .addEventListener('submit', function(event) {
    event.preventDefault();
 
+
    btn.value = 'Sending...';
 
    const serviceID = 'default_service';
    const templateID = 'portfolio_bts';
-
+   let yes;
    emailjs.sendForm(serviceID, templateID, this)
     .then(() => {
-      btn.value = 'Send Email';
-      alert('Votre message à bien été eenvoyé');
+      document.getElementById("send").style.display = "block";
     }, (err) => {
-      btn.value = 'Send Email';
-      alert(JSON.stringify(err));
-    });
-});
+      document.getElementById("erreur").style.display = "block";
+    })
+  }); 
